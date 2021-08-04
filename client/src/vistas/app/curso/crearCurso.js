44e0481969aeda026754fa20a2924f800acae009
Cristian Tito <cristian1997.ctg@gmail.com>
2021-08-04 14:27:19 -0400

    Vista de preinscripcion de curso listo

--- client/src/vistas/app/curso/crearCurso.js
+++ client/src/vistas/app/curso/crearCurso.js
@@ -35,7 +35,7 @@ const CrearCurso = props => {
                             >
                                 <Form>
                                     <Row>
-                                        <Col msd={6}>
+                                        <Col md={6}>
                                             <FormGroup>
                                                 <Label>Seleccione Curso:</Label>
                                                 <Select
@@ -47,27 +47,187 @@ const CrearCurso = props => {
                                                 />
                                             </FormGroup>
                                         </Col>
-                                        <Col sd={6}>
+                                        <Col md={3}>
                                             <FormGroup>
                                                 <Label>Fecha de inicio y fin de curso</Label><br/>
                                                 <Label>Fecha Inicio</Label>
                                                 <div class="form-row">
-                                                    <div class="form-group col-md-10">
+                                                    <div class="form-group ">
                                                         <input type="Date" class="form-control" id="apertura_curso" name="apertura_curso"  placeholder="Fecha" required></input>
                                                     </div>
                                                 </div> 
                                             </FormGroup>
                                         </Col>
-                                        <Col sd={6}>
+                                        <Col md={3}>
                                             <FormGroup><br/>
                                                 <Label>Fecha Fin</Label>
                                                 <div class="form-row">
-                                                    <div class="form-group col-md-10">
+                                                    <div class="form-group ">
                                                         <input type="Date" class="form-control" id="cierre_curso" name="cierre_curso"  placeholder="Fecha" required></input>
                                                     </div>
                                                 </div> 
                                             </FormGroup>
                                         </Col>
+                                    </Row>
+                                    <Row style={{marginTop: "20px"}}>
+                                        <Col mmd={6}>
+                                            <FormGroup>
+                                                <Label>Fecha de pre-inscripcion:</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="fecha_preinscripcion" name="fecha_preinscripcion"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={3}>
+                                            <FormGroup>
+                                                <Label>Fecha examen psicológico</Label><br/>
+                                                <Label>Fecha Inicio</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="apertura_psicol" name="apertura_psicol"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={3}>
+                                            <FormGroup><br/>
+                                                <Label>Fecha Fin</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="cierre_psicol" name="cierre_psicol"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    <Row style={{marginTop: "20px"}}>
+                                        <Col md={3}>
+                                            <FormGroup>
+                                                <Label>Fecha de examen médico</Label><br/>
+                                                <Label>Fecha Inicio</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="apertura_medico" name="apertura_medico"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={3}>
+                                            <FormGroup><br/>
+                                                <Label>Fecha Fin</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="cierre_medico" name="cierre_medico"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        
+                                        <Col md={3}>
+                                            <FormGroup>
+                                                <Label>Fechas examen físico</Label><br/>
+                                                <Label>Fecha Inicio</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="apertura_fisico" name="apertura_fisico"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={3}>
+                                            <FormGroup><br/>
+                                                <Label>Fecha Fin</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="apertura_fisico" name="apertura_fisico"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    <Row style={{marginTop: "20px"}}>
+                                        <Col mmd={6}>
+                                            <FormGroup>
+                                                <Label>Inaguración de curso:</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="inauguracion" name="inauguracion"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={3}>
+                                            <FormGroup>
+                                                <Label>Fechas de Entrenamiento en Tierra</Label><br/>
+                                                <Label>Fecha Inicio</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="apertura_tierra" name="apertura_tierra"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={3}>
+                                            <FormGroup><br/>
+                                                <Label>Fecha Fin</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="cierre_tierra" name="cierre_tierra"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    
+                                    <Row style={{marginTop: "20px"}}>
+                                        <Col md={3}>
+                                            <FormGroup>
+                                                <Label>Fechas de Entrenamiento en Tierra</Label><br/>
+                                                <Label>Fecha Inicio</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="apertura_saltos" name="apertura_saltos"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={3}>
+                                            <FormGroup><br/>
+                                                <Label>Fecha Fin</Label>
+                                                <div class="form-row">
+                                                    <div class="form-group ">
+                                                        <input type="Date" class="form-control" id="cierre_saltos" name="cierre_saltos"  placeholder="Fecha" required></input>
+                                                    </div>
+                                                </div> 
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={6}>
+                                            <FormGroup><br/>
+                                                <Label>Jefe de Curso</Label>
+                                                <Select
+                                                    className="react-select-type"
+                                                    classNamePrefix="react-select-type"
+                                                    name="form-field-name"
+                                                    options={selectInterval}
+                                                    placeholder= "Jefe de curso"
+                                                />
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    <Row style={{marginTop: "20px"}}>
+                                        <Col md={6}>
+                                            <FormGroup>
+                                                <Button color="danger" style={{float: "right"}}>Cancelar</Button>
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={6}>
+                                            <FormGroup>
+                                                <Button color="secondary">Crear</Button>
+                                            </FormGroup>
+                                        </Col>
+                                        
                                     </Row>
                                 </Form>
                             </Formik>
@@ -75,21 +235,6 @@ const CrearCurso = props => {
                     </Card>
                 </div>
             </div>
-           {/* <div class="form-group col-md-6">
-                 <label for="inputFecha4">Fechas de inicio y fin de Curso</label>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputFecha4">Fecha Inicio</label>
-                        <input type="Date" class="form-control" id="apertura_curso" name="apertura_curso"  placeholder="Fecha" required></input>
-                        <span id="fechas_cursos" class="error-message">*Revise las fechas.</span>
-                    </div>
-                    <div class="form-group col-md-6">
-                        <label for="inputFecha4">Fecha Fin</label>
-                        <input type="Date" class="form-control" id="cierre_curso" name="cierre_curso"  placeholder="Fecha"  required></input>
-                    </div>
-                    
-                </div> 
-            </div>*/}
         </div>
     )
 }
