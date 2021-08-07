a9879283ea9f127751b5470b218bb3676bcb7b26
Cristian Tito <cristian1997.ctg@gmail.com>
2021-08-07 16:46:36 -0400

    Vista de cursos creada

--- client/src/vistas/app/curso/verCursos.js
+++ client/src/vistas/app/curso/verCursos.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Card, Label, FormGroup, Button, CardBody, Row, Col } from "reactstrap";
 import { Formik, Form } from "formik";
+import { Link } from 'react-router-dom';
+import ReactTable from 'react-table-v6';
+import 'react-table-v6/react-table.css'
 
 const VerCursos = props => {
     return(
@@ -16,11 +19,53 @@ const VerCursos = props => {
                 <div className="container-data-course">
                     <Card body className="mb-4">
                         <CardBody>
-                            <Row>
-                                <Col md={6}>
+                            <Row style={{backgroundColor: "#ebeff1", padding: "10px", marginBottom: "15px"}}>
+                                <Col md={12}>
                                     <FormGroup>
-                                        <Label>Lista de cursos:</Label>
-                                        
+                                        <Label style={{fontSize: "40px"}}>CURSOS</Label>
+                                        <Link to={'/app/cursos/crear'} key={'crear_curso'} className="link-router" >
+                                            <Button style={{float: "right", marginTop:"11px"}} color="secondary">Crear Curso</Button>
+                                        </Link>
+                                    </FormGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col md={12}>
+                                    <FormGroup>
+                                        <ReactTable 
+                                            data={[]}
+                                            columns={[
+                                                {
+                                                    Header: 'ID',
+                                                    accessor: 'id'
+                                                },{
+                                                    Header: 'Nombre',
+                                                    accessor: 'nombre'
+                                                },{
+                                                    Header: 'Fecha Inicio',
+                                                    accessor: 'fecha inicio'
+                                                },{
+                                                    Header: 'Fecha Fin',
+                                                    accessor: 'fecha fin'
+                                                },{
+                                                    Header: 'Codigo',
+                                                    accessor: 'codigo'
+                                                },{
+                                                    Header: 'Estado',
+                                                    accessor: 'estado'
+                                                },{
+                                                    Header: 'Acciones',
+                                                    accessor: 'acciones'
+                                                }
+                                            ]}
+                                            defaultPageSize={10}
+                                            showPageJump={false}
+                                            showPageSizeOptions={false}
+                                            style={{height: "700px"}}
+                                            className={"react-table-fixed-height"}
+                                            showPagination={true}
+                                            resizable={false}
+                                        />
                                     </FormGroup>
                                 </Col>
                             </Row>
