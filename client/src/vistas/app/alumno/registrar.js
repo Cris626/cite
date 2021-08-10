b8df4baf2e281b9ce822fa74ad5e24585e2c5207
Cristian Tito <cristian1997.ctg@gmail.com>
2021-08-10 19:25:27 -0400

    registrar.js

--- client/src/vistas/app/alumno/registrar.js
+++ client/src/vistas/app/alumno/registrar.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card, Label, FormGroup, Button, CardBody, Row, Col, CustomInput } from "reactstrap";
+import { Formik, Form, Field } from "formik";
+import { SelectField } from '../../../helpers/Select';
+import { connect } from 'react-redux';
+
+const selectCurso = [
+    { label: "Paracaidista", value: "Paracaidista", key: 0 },
+    { label: "Plegador", value: "Plegador", key: 1 },
+    { label: "Salto Libre", value: "Salto Libre", key: 2 },
+]
+
 
 const RegistrarAlumno = props => {
+    const [alumno, setAlumno] = useState({
+        curso: "",
+        num_casco: "",
+        postulante: ""
+    })
+
+    const submitAlumno = value => {
+        console.log(value);
+    };
+
     return(
         <div className="form-create-materia">
             <div className="container-create-materia">
@@ -12,7 +33,56 @@ const RegistrarAlumno = props => {
                     <h3>Ver alumnos</h3>
                 </div>
                 <div className="container-data-materia">
-                    
+                    <Card>
+                        <CardBody>
+                            <Formik
+                                enableReinitialize
+                                initialValues={alumno}
+                                onSubmit={values=>submitAlumno(values)}
+                            >{({values})=>
+                                <Form>
+                                    <Row>
+                                        <Col md={4}>
+                                            <FormGroup>
+                                                <Label>CURSO:</Label>
+                                                <Field 
+                                                    name='curso' 
+                                                    options={selectCurso} 
+                                                    component={SelectField}
+                                                />
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    <Row style={{marginTop:"20px"}}>
+                                        <Col md={4}>
+                                            <FormGroup>
+                                                <Label>Postulante:</Label>
+                                                <Field 
+                                                    name='postulante' 
+                                                    options={selectCurso} 
+                                                    component={SelectField}
+                                                />
+                                            </FormGroup>
+                                        </Col>
+                                        <Col md={4}>
+                                            <FormGroup>
+                                                <Label>Numero de Casco:</Label>
+                                                <Field 
+                                                    className="form-control"
+                                                    name="num_casco"
+                                                    type="number"
+                                                    placeholder="Años de servicio"
+                                                    required
+                                                    values={values.servi}
+                                                />
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    <Button type="submit">REGISTRAR</Button>
+                                </Form>
+                            }</Formik>
+                        </CardBody>
+                    </Card>
                 </div>
             </div>
         </div>
